f55c9e97767e27e50c08fb28070e1b59742c870f
leo Burgi <leonel-nicolas@hotmail.com>
2025-09-12 12:44:36 -0300

    Vista WIP y controladores de Ordenes, Materiales, Facturacion y Usuarios apuntando

---
+++ Controllers/MaterialesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace WebApplication1.Controllers
+{
+    public class MaterialesController : Controller
+    {
+    private IActionResult Wip() => View("~/Views/Shared/Wip.cshtml");
+
+public IActionResult Index() => Wip();
+public IActionResult Details(int id) => Wip();
+public IActionResult Create() => Wip();
+[HttpPost] public IActionResult Create(object _) { return Wip(); }
+public IActionResult Edit(int id) => Wip();
+[HttpPost] public IActionResult Edit(int id, object _) { return Wip(); }
+public IActionResult Delete(int id) => Wip();
+[HttpPost, ActionName("Delete")] public IActionResult DeleteConfirmed(int id) { return Wip(); }
+}
+
+
+
+
+
+
+    
+    }
+
